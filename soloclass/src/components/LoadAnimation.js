d0ab932f399e75df7f68fb776f2532a16d504090
guilhermelp07 <guilpissaia@hotmail.com>
2023-06-14 00:59:42 -0300

    animação de load personalizada

--- soloclass/src/components/LoadAnimation.js
+++ soloclass/src/components/LoadAnimation.js
@@ -7,13 +7,15 @@ export default function LoadAnimation(props){
             visible={props.visible}
             overlayColor="rgba(255,255,255,0.5)"
             animationStyle={styles.lottie}
+            speed={1.5}
+            source={require('../../assets/animations/126444-seeds.json')}
         />
     );
 }
 
 const styles = StyleSheet.create({
     lottie: {
-        width: 100,
-        height: 100
+        width: 160,
+        height: 160
     }
 });
